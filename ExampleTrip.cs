4096ec5b4ed1e41877725bdfdf0778198af97615
Bruce Caslake <bcaslake@outlook.com>
2025-09-17 13:12:40 -0700

    Update ExampleTrip.cs
    added some comments to the file

--- ExampleTrip.cs
+++ ExampleTrip.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
 
+//This is the Example Class for the TripAnalyzerAPP. 
+
 namespace TripAnalyzerAPP
 {
     public class ExampleTrip
     {
+        // These are the Constructors, the accessors and mutators. They are private so only the ExampleTrip Class can modify them
         private string Destination { get; set; }
         private double MilesDriven { get; set; } 
         private double NumberOfGallons { get; set; }
         private double CostOfGas { get; set; }
 
-
+        // This block is the I dont exactly know..
         public ExampleTrip(string destination, double milesDriven, double numberOfGallons, double costOfGas)
         {
             Destination = destination;
@@ -22,6 +25,7 @@ namespace TripAnalyzerAPP
             NumberOfGallons = numberOfGallons;
             CostOfGas = costOfGas;
         }
+        //The following calculates mpg and cost per mile for the example class given.
         public double CalculateMilesPerGallon()
         {
             return MilesDriven / NumberOfGallons;
@@ -30,6 +34,7 @@ namespace TripAnalyzerAPP
         {
             return CostOfGas / NumberOfGallons;
         }
+        // This DisplayInfo is what the main() function in the TripAnalyzerAPP pulls when the code is run.
         public void DisplayInfo()
         {
             WriteLine($"Destination: {Destination}");
